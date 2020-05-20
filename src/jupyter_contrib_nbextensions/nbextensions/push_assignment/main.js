e9e381120919bbb1a4d9d0b1be79c3b345a91a4c
iymarch <iymarch.py@gmail.com>
2020-05-21 01:08:42 +0300

    create nbextension

---
+++ src/jupyter_contrib_nbextensions/nbextensions/push_assignment/main.js
@@ -0,0 +1,47 @@
+define([
+    'base/js/namespace',
+    'jquery'
+], function(
+    Jupyter, $
+) {
+    function load_ipython_extension() {
+
+        let handler = function () {
+            let baseUrl = window.document.body.dataset.baseUrl;
+            let notebookPath = window.document.body.dataset.notebookPath;
+            let urlApiContent = location.host + baseUrl + 'api/contents' + notebookPath; 
+            let username = baseUrl.slice(6, baseUrl.length-1); // pattern "/user/<USERNAME>/"
+
+            let content = $.ajax({
+                type: 'GET',
+                url: urlApiContent,
+                success: (result) => result;
+            });
+      
+            let dataset = {'username': username, 'content': content.responseJSON};
+            $.ajax({
+                type: 'POST',
+                data: {notebook: JSON.stringify(dataset)};
+                url: 'http://10.97.140.6:5000',
+                success: (result) => console.log(200);
+            });
+            
+        };
+
+        let action = {
+            icon: 'fa-comment-o', 
+            help: 'Show an alert',
+            help_index: 'zz',
+            handler: handler
+        };
+        let prefix = 'my_extension';
+        let action_name = 'show-alert';
+
+        let full_action_name = Jupyter.actions.register(action, action_name, prefix); 
+        Jupyter.toolbar.add_buttons_group([full_action_name]);
+    }
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
